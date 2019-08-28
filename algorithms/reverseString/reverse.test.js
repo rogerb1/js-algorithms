c39e8947b3f45c47097e0f56391b6b1cc3c9fe25
rogerdavid2 <rogerb7@uw.edu>
2019-08-28 11:04:15 -0700

    add first test

---
+++ algorithms/reverseString/reverse.test.js
@@ -0,0 +1,6 @@
+const reverseString = require('./reverse.js');
+
+// Testing to see if reverseString is a function 
+test('reverseString is a function', () => {
+    expect(typeof reverseString).toEqual('function');
+});
