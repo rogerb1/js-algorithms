53687b6f0fabaaaca32a77bb66bdc2724ed8b276
rogerdavid2 <rogerb7@uw.edu>
2019-08-28 13:38:24 -0700

    add faster algorithm

--- algorithms/reverseString/reverse.js
+++ algorithms/reverseString/reverse.js
@@ -1,5 +1,17 @@
-function reverseString() {
+// Has a time of 1.2546s
+/*function reverseString(word) {
+    let result = ""
+    for (let i = word.length - 1; i >= 0; i--) {
+        result += word[i];
+    }
+    return result;
+} */
 
+// Faster implementation
+function reverseString(word) {
+    return [...word].reduce((empty, currentChar) => currentChar + empty)
 }
+
+console.log(reverseString('roger'));
 
 module.exports = reverseString;
--- algorithms/reverseString/reverse.test.js
+++ algorithms/reverseString/reverse.test.js
@@ -4,3 +4,17 @@ const reverseString = require('./reverse.js');
 test('reverseString is a function', () => {
     expect(typeof reverseString).toEqual('function');
 });
+
+// Test to see if the function reverses string
+test('should reverse string', () => {
+    expect(reverseString('aeiou')).toEqual('uoiea')
+});
+
+// Test to see if the function number string
+test('should reverse number string ', () => {
+    expect(reverseString('1234')).toEqual('4321')
+});
+
+test('should reverse string of mixed characters', () => {
+    expect(reverseString('AdEfG')).toEqual('GfEdA')
+});
